fd2b624602a5947e6d4e628727280e13fbfbac8e
marcelacoronel <marcelacoronelp@gmail.com>
2020-10-08 17:17:13 -0500

    Funcion obtener links solo leyendo una carpeta

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-module.exports = () => {
-  // ...
-};
+const getLinks = require('./lib/getLinks.js');
+
+const { resolve } = require('path');
+const path = require('path');
+const fs = require('fs');
+//*----DATOS DE ENTRADA DE PRUEBA
+let pathUser = './mdFiles';
+// let pathUser = "./mdFiles/onlyText.md";
+// let pathUser = "./mdFiles/prueba.md";
+// let pathUser = "./mdFiles/texto.txt";
+// let pathUser = "";
+// pathReceived = "";
+// pathReceived = "../../pruebaArchivos/docMD.md";
+//*----FIN DATOS ENTRADA
+
+// *---Validar si ruta existe y es absoluta----
+const validateRoute = ((pathUser) => {
+  if (fs.existsSync(pathUser)) {
+    if (!path.isAbsolute(pathUser)) {
+      const pathResolved = resolve(pathUser);
+      return pathResolved;
+    }
+  } 
+});
+
+
+//*--Función principal MDLINKS--------------
+const mdLinks = (pathUser) => {
+  return new Promise((resolve, reject) => {
+    const validPath = validateRoute(pathUser);
+    getLinks(validPath).then((links)=>{
+    console.log(links);
+    resolve(links);
+    })
+    .catch((err)=>{
+      console.log(err);
+      reject(err)
+    });
+  });
+}
+
+mdLinks(pathUser);
+
+
+
+//*--EJEMPLO PASAR DATOS DESDE LA CONSOLA
+//readFileUser(path.resolve(process.argv[2]))
+//.then(console.log(readFileUser))
+//* ---------------
+
+module.exports = mdLinks;
+module.exports = validateRoute;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,114 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
+const getLinks = require('../lib/getLinks.js');
+const validateRoute = require('../index.js');
+const getFilesMd = require('../lib/getFilesMd.js');
+const readFilesMd = require('../lib/readFilesMd.js');
 
+const path = require('path');
+
+const arrayLinks = [
+  {
+    href: 'https://github.com/markdown-it/markdown-it',
+    text: 'markdown-it',
+    pathFile: 'C:\\Users\\Marcela\\proyecto4\\bog001-md-links\\mdFiles'
+  },
+  {
+    href: 'https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions',
+    text: 'expresiones regulares (<code>RegExp</code>)',
+    pathFile: 'C:\\Users\\Marcela\\proyecto4\\bog001-md-links\\mdFiles'
+  },
+  {
+    href: 'https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions',
+    text: 'expresiones regulares (<code>RegExp</code>)',
+    pathFile: 'C:\\Users\\Marcela\\proyecto4\\bog001-md-links\\mdFiles'
+  },
+  {
+    href: 'https://nodejs.org/es/about/',
+    text: 'Acerca de Node.js - Documentación oficial',
+    pathFile: 'C:\\Users\\Marcela\\proyecto4\\bog001-md-links\\mdFiles'
+  },
+  {
+    href: 'https://medium.com/netscape/a-guide-to-create-a-nodejs-command-line-package-c2166ad0452e',   
+    text: 'https://medium.com/netscape/a-guide-to-create-a-nodejs-command-line-package-c2166ad0452e',   
+    pathFile: 'C:\\Users\\Marcela\\proyecto4\\bog001-md-links\\mdFiles'
+  },
+  {
+    href: 'https://docs.npmjs.com/creating-node-js-modules',
+    text: 'https://docs.npmjs.com/creating-node-js-modules',
+    pathFile: 'C:\\Users\\Marcela\\proyecto4\\bog001-md-links\\mdFiles'
+  }
+  ]
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('Should be a function', () => {
+    expect(typeof mdLinks).toBe('function')
   });
 
+  it('Should be return absolute path', () => {
+    expect(validateRoute('./mdFiles')).toBe('C:\\Users\\Marcela\\proyecto4\\bog001-md-links\\mdFiles')
+  });
+
+  it.skip('Should be return a error message', () => {
+    const route = './mdFiles/texto.txt';
+    const errCode = 'ENOTDIR';
+    return mdLinks(route).catch(err => {
+      expect(err.code).toBe(errCode)
+    });
+  });
+  
+  it.skip('Should throw an error ', () => {
+      const route = './mdFiles/texto.txt';
+      expect(mdLinks(route)).toThrow();
+    });
+
+    // return expect(mdLinks(route)).rejects.toBe(errCode)
+    
+
 });
+
+
+
+describe('getLinks', () => {
+  it('Should be a function', () => {
+    expect(typeof getLinks).toBe('function')
+  });
+  it.skip('Should be....', () => {
+    expect(getLinks('./mdFiles')).toBe('object')
+  });
+});
+
+describe('getFilesMd', () => {
+  it('Should be a function', () => {
+    expect(typeof getFilesMd).toBe('function')
+  });
+  it('Should be return array whit 2 elements', () => {
+    getFilesMd('./mdFiles').then((files)=>{
+      expect(files.length).toBe(2);
+    })
+  });
+  it('Should be return array with 2 files includes its path', () => {
+    const filesMd = [
+    'C:\\Users\\Marcela\\proyecto4\\bog001-md-links\\mdFiles\\onlyText.md',
+    'C:\\Users\\Marcela\\proyecto4\\bog001-md-links\\mdFiles\\prueba.md'
+    ];
+    getFilesMd('./mdFiles').then((files)=>{
+      expect(files).toBeEqual(filesMd);
+    })
+  });
+  
+});
+
+describe('readFilesMd', () => {
+  it('Should be a function', () => {
+    expect(typeof readFilesMd).toBe('function')
+  });
+  it.skip('Should be return links array', () => {
+    return readFilesMd('./mdFiles').then((links)=>{
+      console.log(links);
+      expect(links).toBeEqual(arrayLinks);
+      })
+  });
+
+  
+});
+
