e0ea4ce2ec8771589452eac4e71c9a7e8e1d49f8
marcelacoronel <marcelacoronelp@gmail.com>
2021-03-11 21:14:20 -0500

    Modificación función mdLinks, refactorización función getLinks para leer carpetas de manera recursiva, modificación archivo cli para acceder a la función principal

--- index.js
+++ index.js
@@ -1,60 +1,47 @@
 const getLinks = require('./lib/getLinks.js');
 const validateLinks = require('./lib/validateLinks');
-
-
 const { resolve } = require('path');
 const path = require('path');
 const fs = require('fs');
 
-//*----DATOS DE ENTRADA DE PRUEBA ---*/
-// let pathUser = './mdFiles';
-// let options = { validate: true };
-// let options = { validate: false };
-//*----FIN DATOS ENTRADA ---*/
 
-// *---Validar si ruta existe y es absoluta----
-const validateRoute = ((pathUser) => {
-  if (fs.existsSync(pathUser)) {
-    if (!path.isAbsolute(pathUser)) {
-      const pathResolved = resolve(pathUser);
-      return pathResolved;
-    }
-  } 
-});
 
-//**--Función principal MDLINKS----- */
-const mdLinks = (pathUser,options={})=> {
-  // return new Promise((resolve, reject) => {
-    const validPath = validateRoute(pathUser);
-    return getLinks(validPath)
-    .then((links)=>{
-      if (options.validate) {
-        return validateLinks(links);
-      }
-      // else {
-        // console.log("respueta de getlinks");
-        // console.log(links);
-        return links;
-      // }
+// Función principal MDLINKS
+// const mdLinks = (pathUser, options = {}) => {
+//       return getLinks(pathUser)
+//       .then((links)=>{
+//         if (options.validate) {
+//           return validateLinks(links);
+//         }
+//         else
+//           return links;
+//       })
+//       .catch((err)=>{
+//         console.log(err.message);
+//       });
+//   }
+
+  const mdLinks = (pathUser, options = {}) => {
+    return new Promise((resolve, reject) => {
+      getLinks(pathUser)
+      .then((links)=>{
+        if (options.validate) {
+          resolve(validateLinks(links));
+        }
+        else
+          resolve(links);
+      })
+      .catch((err)=>{
+        console.log(err.message);
+      });
     })
-    .catch((err)=>{
-      console.log(err.message);
-      // reject(err)
-    });
+
     
-  // });
 }
 
+//Prueba para ingreso de ruta y ejecutar la función mdLinks
+// mdLinks(process.argv[2])
+//   .then((resp) => console.log(resp));
 
-//**---Prueba de mdlinks---- */
-// mdLinks(pathUser,options)
-// .then((resp)=>console.log(resp));
+module.exports = mdLinks;
 
-mdLinks(process.argv[2])
-.then((resp)=>console.log(resp));
-
-
-
-module.exports = {mdLinks,
-  validateRoute};
-// module.exports = validateRoute;
